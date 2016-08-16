fc9b7a10e16705fd6f507e003b47d28c1db7bc73
Andrew Dolgert <adolgert@uw.edu>
2016-08-16 07:29:31 -0700

    added tests. created sphinx docs.

---
+++ tests/test_settings.py
@@ -0,0 +1,17 @@
+import pytest
+import provda
+
+
+class TestBasic(object):
+    def test_read(self):
+        parameters = provda.get_parameters("provda.tests.sample")
+        provda.read_json(open("sample.settings"))
+        assert parameters["risk"]=="smoking"
+
+        subparams = provda.get_parameters("provda.tests.submodule")
+        assert subparams["algorithm"]=="steepest descent"
+
+    def test_hierarchical(self):
+        parameters = provda.get_parameters("provda.tests.sample")
+        provda.read_json(open("sample.settings"))
+        assert parameters["runlimit"]==10
